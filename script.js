3f35c37f86a5b47f12069042e6512994b725f888
Victor Cordeiro <viktorjooji@gmail.com>
2023-09-17 21:09:12 -0300

    ver 1.0.1

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ let statusElement = document.querySelector("#status");
 
 let alfabeto = "abcdefghijklmnopqrstuvwxyz".split("");
 let alfabetoAlternativo = "snvfrghjoklzqmpawtdyibecux".split("");
+let numeros = "1234567890".split("");
+let numerosAlternativo = "2345678901".split("");
 
 function criptografar(){
-    let valorInputPrincipal = inputPrincipal.value;
+    let valorInputPrincipal = inputPrincipal.value.toLowerCase();
     let arr = [];
     for(let i = 0; i < valorInputPrincipal.length; i++){
-        if(!(valorInputPrincipal[i] === " ")){
-            let indiceLetraAtual = alfabeto.indexOf(valorInputPrincipal[i]);
+        let indiceLetraAtual = alfabeto.indexOf(valorInputPrincipal[i]);
+        let indiceNumeroAtual = numeros.indexOf(valorInputPrincipal[i]);
+        if(indiceLetraAtual >=0 && indiceLetraAtual <=25){
             arr.push(alfabetoAlternativo[indiceLetraAtual]);
-        } else{
-            arr.push(" ");
+        } else if(indiceNumeroAtual >= 0 && indiceNumeroAtual <= 10){
+            arr.push(numerosAlternativo[indiceNumeroAtual])
         }
-
+        else{
+            arr.push(valorInputPrincipal[i]);
+        }
     }
     arr = arr.join("")
     statusElement.textContent = arr;
@@ -22,24 +27,28 @@ function criptografar(){
 }
 
 function descriptografar(){
-    let valorInputPrincipal = inputPrincipal.value;
+    let valorInputPrincipal = inputPrincipal.value.toLowerCase();
     let arr = [];
     for(let i = 0; i < valorInputPrincipal.length; i++){
-        if(!(valorInputPrincipal[i] === " ")){
-            let indiceLetraAtual = alfabetoAlternativo.indexOf(valorInputPrincipal[i]);
+        let indiceLetraAtual = alfabetoAlternativo.indexOf(valorInputPrincipal[i]);
+        let indiceNumeroAtual = numerosAlternativo.indexOf(valorInputPrincipal[i]);
+        if(indiceLetraAtual >=0 && indiceLetraAtual <=25){
             arr.push(alfabeto[indiceLetraAtual]);
-        } else{
-            arr.push(" ");
+        } else if(indiceNumeroAtual >= 0 && indiceNumeroAtual <= 10){
+            arr.push(numeros[indiceNumeroAtual])
         }
-
+        else{
+            arr.push(valorInputPrincipal[i]);
+        }
     }
     arr = arr.join("")
     statusElement.textContent = arr;
+    
 
 }
 
 inputPrincipal.addEventListener("input", ()=>{
-    let valorInputPrincipal = inputPrincipal.value;
+    let valorInputPrincipal = inputPrincipal.value.toLowerCase();
 
     if(valorInputPrincipal.length > 0){
         statusElement.textContent = "Something is being typed"        
